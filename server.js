f06d394d0ed89cdc200e928f1eb415419c9c1ec0
William Hirsch <Williamhirschh@gmail.com>
2022-11-15 19:22:46 -0600

    reworked some js and cleaned things up

--- server.js
+++ server.js
@@ -17,34 +17,35 @@ app.use(express.static('public'));
 
 
 
-
+//Gets for routes
 app.get('/api/notes', (req, res) => {
     res.json(allNotes.slice(1));
 });
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
-
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
-
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+//---------------------------------------//
+
+
+
+
+
+//Creates new note
 function createNewNote(body, notesArray) {
     const newNote = body;
     if (!Array.isArray(notesArray))
         notesArray = [];
-    
     if (notesArray.length === 0)
         notesArray.push(0);
-
     body.id = notesArray[0];
     notesArray[0]++;
-
     notesArray.push(newNote);
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
@@ -52,16 +53,21 @@ function createNewNote(body, notesArray) {
     );
     return newNote;
 }
+//---------------------------------------//
 
+
+//posts new note when creates
 app.post('/api/notes', (req, res) => {
     const newNote = createNewNote(req.body, allNotes);
     res.json(newNote);
 });
+//---------------------------------------//
 
+
+//deletes note
 function deleteNote(id, notesArray) {
     for (let i = 0; i < notesArray.length; i++) {
         let note = notesArray[i];
-
         if (note.id == id) {
             notesArray.splice(i, 1);
             fs.writeFileSync(
@@ -71,10 +77,13 @@ function deleteNote(id, notesArray) {
 
             break;
         }
-    }
+     }
 }
+//---------------------------------------//
 
 
+
+//makes server live
 app.listen(PORT, () => {
     console.log(`API server now on port localhost:${PORT}!`);
 });
